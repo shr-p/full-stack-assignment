d6bfb912a987a9e9ea87dc6227e163c31053cb84
sree <minnualankar@gmail.com>
2023-05-12 15:28:08 +0530

    First changes made

--- index.js
+++ index.js
@@ -17,27 +17,49 @@ const QUESTIONS = [{
 const SUBMISSION = [
 
 ]
+app.get('/', (req, res)=>{
+  res.send("Hello World")
+})
 
 app.post('/signup', function(req, res) {
   // Add logic to decode body
   // body should have email and password
+  let email = req.body().email
+  let password = req.body().password
 
-
+  let flag = false
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-
-
+  for (var i = 0; i < USERS.length; i++) {
+    if(email == USERS[i].email){
+      flag = true
+      break
+    }
+  }
+  if(flag === false) USERS.push({email : email, password : password})
   // return back 200 status code to the client
+  res.sendStatus(200)
   res.send('Hello World!')
 })
 
 app.post('/login', function(req, res) {
   // Add logic to decode body
   // body should have email and password
+  let email = req.body().email
+  let password = req.body().password
 
+  let flag = false
   // Check if the user with the given email exists in the USERS array
   // Also ensure that the password is the same
-
-
+  for (var i = 0; i < USERS.length; i++) {
+    if(email == USERS[i].email){
+      if(password == USERS[i].password){
+        flag = true
+        break
+      }
+    }
+  }
+  if(flag === true) res.statusCode(200);
+  else res.sendStatus(401)
   // If the password is the same, return back 200 status code to the client
   // Also send back a token (any random string will do for now)
   // If the password is not the same, return back 401 status code to the client
@@ -47,7 +69,9 @@ app.post('/login', function(req, res) {
 })
 
 app.get('/questions', function(req, res) {
-
+  for(var i = 0; i < QUESTIONS.length; i++){
+    res.send(QUESTIONS[i])
+  }
   //return the user all the questions in the QUESTIONS array
   res.send("Hello World from route 3!")
 })
